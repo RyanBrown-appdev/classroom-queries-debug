685b52d1365dff28a65e32c129971aee1a7fa1c7
RyanBrown55 <Rbrown55@uchicago.edu>
2021-10-29 22:51:40 +0000

    Done 100

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,18 +1,14 @@
 class DepartmentsController < ApplicationController
   def index
     matching_departments = Department.all
-
     @list_of_departments = matching_departments.order({ :created_at => :desc })
-
     render({ :template => "departments/index.html.erb" })
   end
 
   def show
     the_id = params.fetch("path_id")
-
     matching_departments = Department.where({ :id => the_id })
-
-    @the_department = matching_departments
+    @the_department = matching_departments.at(0)
 
     render({ :template => "departments/show.html.erb" })
   end
